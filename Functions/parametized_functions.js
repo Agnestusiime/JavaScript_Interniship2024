4b063c4608d8a4286ebdd2a31211437e4b4a30a2
samueltexa <woolardsamuels@gmail.com>
2024-06-21 12:05:02 +0300

    update parametized_functions.js

--- Functions/parametized_functions.js
+++ Functions/parametized_functions.js
@@ -32,10 +32,33 @@ function calculate_student_grade(stduent_name, student_class, marks){
 console.log(calculate_student_grade("John", "SS3", 90));
 
 // Returning parametized fumction with initial parameters
-function grade_student(english_marks = 70, biology_marks){
+const grade_student=(english_marks = 70, biology_marks)=>{
     const total_marks = english_marks + biology_marks;
     return{ total_marks,
         english_marks,
         biology_marks}
 }
 console.log(grade_student(undefined, 80));
+
+
+// function inside a function(clojure)
+const staff_members_details = ()=>{
+    const head_teacher_details = ()=>{
+        let head_teacher_password = "1234";
+        const head_teacher_name = ["Alinda Samuel"];
+        return head_teacher_name, head_teacher_password;
+    };
+    const other_staff_details = ()=>{
+        const teachers_names = ["John", "Jane", "Doe"];
+        return teachers_names;
+    };
+    const head_teacher = head_teacher_details();
+    const other_staff = other_staff_details();
+    const every_one_details = {
+        head_teacher,
+        other_staff
+    };
+    return every_one_details;
+}
+// staff_members_details
+console.log(staff_members_details());
